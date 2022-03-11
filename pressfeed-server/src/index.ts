79a19fc8c43cecc6b0c1d6c0a3d9c4ec09724039
Kye <kyelindholm@gmail.com>
2022-03-11 11:20:46 -0500

    built and tested initial server-side file structure

--- pressfeed-server/src/index.ts
+++ pressfeed-server/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+const { getStories } = require('../controllers/getStories');
+const NYT_API_KEY: string = require('../../config.ts').NYT_API_KEY;
 const app = express();
 const PORT: number = 8000;
 
-console.log('Hello, World!');
-
-
+app.get('/', async (req, res) => {
+  let responseData = await getStories(req.query.section, NYT_API_KEY);
+  res.send(responseData);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at http:/localhost:${PORT}`);
