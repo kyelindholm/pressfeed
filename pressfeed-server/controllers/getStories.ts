11a4d6d156620edd7e8269fc1333b75602852ed8
Kye <kyelindholm@gmail.com>
2022-03-12 15:16:03 -0500

    now rendering more stories

--- pressfeed-server/controllers/getStories.ts
+++ pressfeed-server/controllers/getStories.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getStories = async (section: string = 'home', key: string) => {
   let data = await axios.get(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${key}`);
-  return(data.data.results.slice(0, 10));
+  return(data.data.results);
 }
 
 module.exports = { getStories };
