25ceba26de2d9f7ff98ec7535760f46f5f385448
Kye <kyelindholm@gmail.com>
2022-03-12 09:00:26 -0500

    integrated materialUI and started some basic styling

--- pressfeed-client/src/components/Feed.tsx
+++ pressfeed-client/src/components/Feed.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Article from './Article';
+import { Container, Grid } from '@mui/material'
 
 interface Props {
   articles: Array<any> | void | undefined
@@ -10,14 +11,16 @@ let articleKey: number = 0;
 const Feed: React.FC<Props> = ({articles}) => {
   if (articles !== undefined) {
     return (
-      <div>
+      <Container maxWidth="md">
+        <Grid container spacing={4}>
         {articles.map((article) => {
           articleKey ++;
           return (
             <Article article={article} key={articleKey}/>
           )
         })}
-      </div>
+        </Grid>
+      </Container>
     )
   } else {
     return <div/>;
