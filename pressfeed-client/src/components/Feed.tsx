bfa71c7a80c6a57631efd846506fe781a51ba487
Kye <kyelindholm@gmail.com>
2022-03-13 20:39:13 -0400

    fixed rendering bug when an image associated with article isn't present

--- pressfeed-client/src/components/Feed.tsx
+++ pressfeed-client/src/components/Feed.tsx
@@ -13,7 +13,7 @@ const Feed: React.FC<Props> = ({articles}) => {
   if (articles !== undefined) {
     return (
       <Container maxWidth="md" className={classes.container}>
-        <Grid container spacing={6}>
+        <Grid container spacing={6} columns={3}>
         {articles.map((article) => {
           articleKey ++;
           return (
